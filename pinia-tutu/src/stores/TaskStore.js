bf6bda111dc450d904c482039deda578d2d3be75
NorelhoudaB <norelhouda.brioua@univ-constantine2.dz>
2025-07-17 16:59:32 +0100

    adding the async

--- pinia-tutu/src/stores/TaskStore.js
+++ pinia-tutu/src/stores/TaskStore.js
@@ -20,27 +20,51 @@ export const useTaskStore = defineStore("TaskStore", {
     }
 
 
-    },    actions: {
-        async getTasks() {
-            this.isLoading = true
-            const response = await fetch('http://localhost:3000/tasks')
-            const data = await response.json()
-            this.tasks = data
-            this.isLoading=false
-        },
-        addTask(task) {
-            this.tasks.push(task)
-    },deleteTask(id) {
-        this.tasks=this.tasks.filter(t =>{
-            
-            return t.id !== id})
 },
-toggleCompleted(id) {
-const task =this.tasks.find(t => t.id === id)
-task.Completed = !task.Completed
+actions: {
+    async getTasks() {
+        this.isLoading = true
+        const response = await fetch('http://localhost:3000/tasks')
+        const data = await response.json()
+        this.tasks = data
+        this.isLoading = false
+    },
+    async addTask(task) {
+        this.tasks.push(task)
 
-}
+        const res = await fetch('http://localhost:3000/tasks', {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (res.err) {
+            console.error('Error adding task:', res.err)
+        }
+    },
+    async deleteTask(id) {
+        this.tasks = this.tasks.filter(t => t.id !== id)
+        const res = await fetch('http://localhost:3000/tasks/' + id, {
+            method: 'DELETE',
+        })
+        if (res.err) {
+            console.error('Error deleting task:', res.err)
+        }
+    },
+    async toggleCompleted(id) {
+        const task = this.tasks.find(t => t.id === id)
+        task.Completed = !task.Completed
+        const res = await fetch('http://localhost:3000/tasks/' + id, {
+            method: 'PATCH',
+            body: JSON.stringify({ Completed: task.Completed }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (!res.ok) {
+            console.error('Error toggling completed:', await res.text())
+        }
     }
-    
+}
 });
-                                             
